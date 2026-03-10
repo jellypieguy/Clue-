a716d84ee92dd229b5d9a6b72c41fc966627e1d6
grace <thunshweyeehtoo@gmail.com>
2026-03-10 00:11:11 +0000

    update

---
+++ My project/Assets/Scripts/Turn System.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TurnSystem : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     Debug.Log("Unity script works!");
+    }
+}
